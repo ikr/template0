35d03cc3f078629b09263589cc51e7f1a78a3f05
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-04-08 16:16:51 +0200

    Depend on PHPUnit

--- guest_os_install.sh
+++ guest_os_install.sh
@@ -4,3 +4,5 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 # composer packages installation, DB schema creation, etc.
+
+composer install --dev
