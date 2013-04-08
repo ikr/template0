66f2be4cabe6bb7cfaf2d86bfb06b7dc18fc86ad
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-04-08 20:16:23 +0200

    Hello from Silex

--- www/index.php
+++ www/index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Application;
+
+$app = new Application;
+
+$app->get('/', function () use ($app) {
+    return 'Превед!';
+});
+
+$app->run();
