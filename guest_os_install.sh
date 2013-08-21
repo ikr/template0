bfd6d70ca3d3ab4c926bb132550b7ee46269bcd9
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-08-21 17:58:40 +0200

    No more guest_os_install.sh; all must be done via SaltStack

--- guest_os_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-
-# composer packages installation, DB schema creation, etc.
-
-composer install --dev
